121f02317c8f2ff061da9f7d698680b4f0ad3f88
Saveliy Dmitriyevich <neron4110@gmail.com>
2025-09-10 00:14:28 +0300

    Day 02: калькулятор v2.1

--- PythonBasic 1.py
+++ PythonBasic 1.py
@@ -8,6 +8,14 @@ def multiply(num_1, num_2):
     return num_1 * num_2
 def divide(num_1, num_2):
     return num_1 / num_2
+def square(num_1):
+    return num_1 ** 2
+def cube(num_1):
+    return num_1 ** 3
+def mod(num_1, num_2):
+    return num_1 % num_2
+def sqrt(num_1):
+    return num_1 ** 0.5
 def get_number():
     while True:
         num = input('Enter your number ')
@@ -17,10 +25,12 @@ def get_number():
             print("Invalid number, try again")
 def memory(num_1, num_2, final_result):
     memory_action.append(f'{num_1} {action} {num_2} = {final_result} ')
+def memory_another(num_1, final_result):
+    memory_action.append(f'{action} {num_1} = {final_result} ')
 def memory_print():
     print(*memory_action, sep="\n")
 
-action_list = ['add', 'subtract', 'multiply', 'divide', 'quit', 'memory']
+action_list = ['add', 'subtract', 'multiply', 'divide', 'quit', 'memory', 'sqrt', 'square','cube', 'mod']
 action = ''
 while action != 'quit':
     print()
@@ -29,6 +39,21 @@ while action != 'quit':
     if action == 'quit':
         print('Thank you for using this program. Goodbye!')
         break
+    elif action == 'sqrt':
+        num_1 = get_number()
+        final_result = sqrt(num_1)
+        print(f'result = {final_result}')
+        memory_another(num_1, final_result)
+    elif action == 'square':
+        num_1 = get_number()
+        final_result = square(num_1)
+        print(f'result = {final_result}')
+        memory_another(num_1, final_result)
+    elif action == 'cube':
+        num_1 = get_number()
+        final_result = cube(num_1)
+        print(f'result = {final_result}')
+        memory_another(num_1, final_result)
     elif action == 'memory':
         memory_print()
     elif action not in action_list:
@@ -46,6 +71,10 @@ while action != 'quit':
             memory(num_1, num_2, final_result)
         elif action == 'multiply':
             final_result = multiply(num_1, num_2)
+            print(f'result = {final_result}')
+            memory(num_1, num_2, final_result)
+        elif action == 'mod':
+            final_result = mod(num_1, num_2)
             print(f'result = {final_result}')
             memory(num_1, num_2, final_result)
         elif action == 'divide':
