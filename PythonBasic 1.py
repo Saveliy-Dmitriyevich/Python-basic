68027a8e88d26ade470e2a6c8d7e7fe05fe7ae11
Saveliy Dmitriyevich <neron4110@gmail.com>
2025-09-09 23:51:52 +0300

    Day 02: калькулятор v2 (функции, float, новые операции, история)

--- PythonBasic 1.py
+++ PythonBasic 1.py
@@ -1,37 +1,59 @@
-action_list = ['add', 'subtract', 'multiply', 'divide', 'quit']
+memory_action = []
+
+def add(num_1, num_2):
+    return num_1 + num_2
+def subtract(num_1, num_2):
+    return num_1 - num_2
+def multiply(num_1, num_2):
+    return num_1 * num_2
+def divide(num_1, num_2):
+    return num_1 / num_2
+def get_number():
+    while True:
+        num = input('Enter your number ')
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid number, try again")
+def memory(num_1, num_2, final_result):
+    memory_action.append(f'{num_1} {action} {num_2} = {final_result} ')
+def memory_print():
+    print(*memory_action, sep="\n")
+
+action_list = ['add', 'subtract', 'multiply', 'divide', 'quit', 'memory']
 action = ''
 while action != 'quit':
     print()
     print(*action_list)
-    action = input('What would you like to do? ')
+    action = input('What would you like to do? ').strip().lower()
     if action == 'quit':
-        print('Goodbye')
+        print('Thank you for using this program. Goodbye!')
         break
-    if action not in action_list:
+    elif action == 'memory':
+        memory_print()
+    elif action not in action_list:
         print('Invalid action')
     else:
-        num_1 = input('enter first number: ')
-        if not num_1.isdigit():
-            print('Invalid number 1')
-            continue
-        num_2 = input('enter second number: ')
-        if not num_2.isdigit():
-            print('Invalid number 2')
-            continue
-
-        num_1 = int(num_1)
-        num_2 = int(num_2)
-
+        num_1 = get_number()
+        num_2 = get_number()
         if action == 'add':
-            print('result = ', num_1 + num_2)
+            final_result =  add(num_1, num_2)
+            print(f'result = {final_result}')
+            memory(num_1, num_2, final_result)
         elif action == 'subtract':
-            print('result = ', num_1 - num_2)
+            final_result = subtract(num_1, num_2)
+            print(f'result = {final_result}')
+            memory(num_1, num_2, final_result)
         elif action == 'multiply':
-            print('result = ', num_1 * num_2)
+            final_result = multiply(num_1, num_2)
+            print(f'result = {final_result}')
+            memory(num_1, num_2, final_result)
         elif action == 'divide':
             if num_2 == 0:
                 print('Division by zero')
             else:
-                print('result = ', num_1 / num_2)
+                final_result = divide(num_1, num_2)
+                print(f'result = {final_result}')
+                memory(num_1, num_2, final_result)
 
 
