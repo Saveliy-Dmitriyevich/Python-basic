d7d7c0f8aa143a40aee385ae78d97784d78d7881
Saveliy Dmitriyevich <neron4110@gmail.com>
2025-09-15 23:56:40 +0300

    Day 03: калькулятор v3

--- PythonBasic 1.py
+++ PythonBasic 1.py
@@ -7,7 +7,10 @@ def subtract(num_1, num_2):
 def multiply(num_1, num_2):
     return num_1 * num_2
 def divide(num_1, num_2):
-    return num_1 / num_2
+    try:
+        return num_1 / num_2
+    except ZeroDivisionError:
+        print("Division by zero, try again")
 def square(num_1):
     return num_1 ** 2
 def cube(num_1):
@@ -18,7 +21,9 @@ def sqrt(num_1):
     return num_1 ** 0.5
 def get_number():
     while True:
-        num = input('Enter your number ')
+        num = input('Enter your number (or MR) ')
+        if num.strip().upper() == "MR":
+            return settings["memory"]
         try:
             return float(num)
         except ValueError:
@@ -29,9 +34,29 @@ def memory_another(num_1, final_result):
     memory_action.append(f'{action} {num_1} = {final_result} ')
 def memory_print():
     print(*memory_action, sep="\n")
+def precision():
+    circle = int(input('Enter number of round '))
+    settings['precision'] = circle
+    print(f'number of round: {circle}')
+settings ={
+    "precision": 6,
+    "memory": 0,
+    "last_result": 0
+}
+operations = {
+    "add": (add, 2 ),
+    "subtract": (subtract, 2),
+    "multiply": (multiply, 2),
+    "divide": (divide, 2),
+    "mod": (mod, 2),
+    "sqrt": (sqrt, 1),
+    "square": (square, 1),
+    "cube": (cube, 1),
+}
 
-action_list = ['add', 'subtract', 'multiply', 'divide', 'quit', 'memory', 'sqrt', 'square','cube', 'mod']
+action_list = ['add', 'subtract', 'multiply', 'divide', 'quit', 'memory', 'sqrt', 'square','cube', 'mod', 'precision','M+','MR','MC']
 action = ''
+
 while action != 'quit':
     print()
     print(*action_list)
@@ -39,50 +64,30 @@ while action != 'quit':
     if action == 'quit':
         print('Thank you for using this program. Goodbye!')
         break
-    elif action == 'sqrt':
-        num_1 = get_number()
-        final_result = sqrt(num_1)
-        print(f'result = {final_result}')
-        memory_another(num_1, final_result)
-    elif action == 'square':
-        num_1 = get_number()
-        final_result = square(num_1)
-        print(f'result = {final_result}')
-        memory_another(num_1, final_result)
-    elif action == 'cube':
-        num_1 = get_number()
-        final_result = cube(num_1)
-        print(f'result = {final_result}')
-        memory_another(num_1, final_result)
-    elif action == 'memory':
+    elif action in operations:
+        func, args = operations[action]
+        if args == 1:
+            num_1 = get_number()
+            final_result = func(num_1)
+            print(round(final_result, settings['precision']))
+            settings['last_result'] = final_result
+            memory_another(num_1, final_result)
+        elif args == 2:
+            num_1 = get_number()
+            num_2 = get_number()
+            final_result = func(num_1, num_2)
+            print(round(final_result, settings['precision']))
+            settings['last_result'] = final_result
+            memory(num_1, num_2, final_result)
+    elif action.strip().lower() == 'precision':
+        precision()
+    elif action.strip().lower() == 'memory':
         memory_print()
+    elif action.strip().lower() == 'm+':
+        settings['memory'] = settings['memory'] + settings['last_result']
+    elif action.strip().lower() == 'mc':
+        settings['memory'] = 0
+        settings['last_result'] = 0
     elif action not in action_list:
         print('Invalid action')
-    else:
-        num_1 = get_number()
-        num_2 = get_number()
-        if action == 'add':
-            final_result =  add(num_1, num_2)
-            print(f'result = {final_result}')
-            memory(num_1, num_2, final_result)
-        elif action == 'subtract':
-            final_result = subtract(num_1, num_2)
-            print(f'result = {final_result}')
-            memory(num_1, num_2, final_result)
-        elif action == 'multiply':
-            final_result = multiply(num_1, num_2)
-            print(f'result = {final_result}')
-            memory(num_1, num_2, final_result)
-        elif action == 'mod':
-            final_result = mod(num_1, num_2)
-            print(f'result = {final_result}')
-            memory(num_1, num_2, final_result)
-        elif action == 'divide':
-            if num_2 == 0:
-                print('Division by zero')
-            else:
-                final_result = divide(num_1, num_2)
-                print(f'result = {final_result}')
-                memory(num_1, num_2, final_result)
-
 
